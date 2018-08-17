4dba615cd90f71091ea9411b44a0c785e1d7820b
Kevin Dietz <kevinknowscs@gmail.com>
2018-08-16 23:22:07 -0600

    Table of contents now generated dynamically

---
+++ scripts/toc.js
@@ -0,0 +1,33 @@
+
+onDocumentReady(function () {
+  getAjax(globals.baseUrl + '/data/toc.json', function (data) {
+    toc = JSON.parse(data);
+    var headingEl, labelEl, sectionEl, anchorEl;
+    var navRootEl = document.getElementsByClassName('sidebar-area')[0];
+    navRootEl.innerHTML = '';
+
+    toc.sections.forEach(function (sectionData) {
+      sectionEl = document.createElement('section');
+      if (sectionData.hide) {
+        return;
+      }
+
+      if (sectionData.label) {
+        headingEl = document.createElement('h1');
+        headingEl.innerHTML = sectionData.label;
+        sectionEl.appendChild(headingEl);
+      }
+
+      sectionData.items.forEach(function (itemData) {
+        anchorEl = document.createElement('a');
+        anchorEl.href = itemData.href.startsWith('http') ? itemData.href : globals.baseUrl + '/' + itemData.href;
+        anchorEl.target = itemData.target || '_self';
+        anchorEl.appendChild(document.createTextNode(itemData.label));
+
+        sectionEl.appendChild(anchorEl);
+      });
+
+      navRootEl.appendChild(sectionEl);
+    });
+  });
+});
